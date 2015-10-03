b57316715a3cd38476ae0cfaae3d12d864d35fa3
vjdavid <vjdavid@gmail.com>
2015-10-03 16:17:35 -0700

    added method to show article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -50,7 +50,9 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+      $article = Article::find($id);
+
+      return response()->json($article);
     }
 
     /**
